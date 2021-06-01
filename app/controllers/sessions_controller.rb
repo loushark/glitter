faf2d0f89446d18fe70a33ca04ff76d4737ec374
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-06-01 16:12:14 +0100

    use can logout, session destroyed, user can login, session created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "username or password is invalid"
+      render "new"
+    end
   end
 
   def login
   end
 
   def destroy
-    redirect_to root
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -10,4 +10,31 @@ RSpec.feature "Authentication of a user", type: :feature do
     expect(page).to have_link("Logout")
     expect(page).to have_content("Welcome Sparkle_Dino")
   end
+
+  scenario "User can login" do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in "Username", with: "Sparkle_Dino"
+    fill_in "Password", with: "Pa55w0rd"
+    click_button "Submit"
+    click_link 'Logout'
+    click_link 'Login'
+    fill_in "Username", with: "Sparkle_Dino"
+    fill_in "Password", with: "Pa55w0rd"
+    click_button "Login"
+    expect(page).to have_link("Logout")
+    expect(page).to have_content("Welcome Sparkle_Dino")
+  end
+
+  scenario "User can logout" do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in "Username", with: "Sparkle_Dino"
+    fill_in "Password", with: "Pa55w0rd"
+    click_button "Submit"
+    click_link 'Logout'
+    expect(page).to have_link("Sign Up")
+    expect(page).to have_link("Login")
+    expect(page).to have_content("Welcome Guest")
+  end
 end
